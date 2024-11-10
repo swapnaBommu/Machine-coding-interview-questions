f9405ecc6bf2932f01c4e499058cbabac4b82276
swapnaBommu <swapnabommu8143@gmail.com>
2024-11-10 09:51:29 +0530

    Question 1- Click Event Example

--- JavaScript-Interview-test-questions/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener(onload,function(){
-    const btn = document.getElementById("smart-btn");
-
-    btn.addEventListener("click", function(){
-        console.log("Hello World");
-    })
-})
