c28adcc4e6a7da38590eecf8463baedd208baf5c
swapnaBommu <swapnabommu8143@gmail.com>
2026-02-19 19:24:43 +0530

    created theme button using contextAPi

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+
 import { CounterProvider } from './CounterContext'
 import Counter from './Counter'
+import { UseTheme } from './ThemeContext.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const { theme, toggleTheme} = UseTheme();
   return (
-    <>
+    <div >
+      <button onClick={toggleTheme}>
+        Switch to {theme === "light" ? "Dark" : "Light"} Mode
+    </button>
       <CounterProvider>
         <Counter />
       </CounterProvider>
-    </>
+    </div>
   )
 }
 
--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { useCounter } from './CounterContext.jsx';
 import { useState } from 'react';
 
+
 const Counter = () => {
     const {count, increment, decrement, incrementBy,reset} = useCounter();
+    
     const [incrementByVal, setIncrementByVal] = useState();
   return (
     <div>
+      
       <h2>Count: {count}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
